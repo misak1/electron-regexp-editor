f95d0b38d1bb13f04ec7091159f8c47481bf2d25
m-shibata <misaki.pink@gmail.com>
2017-01-05 10:26:58 +0900

    リリース用に書き換え

--- main.js
+++ main.js
@@ -52,8 +52,8 @@ app.on('ready', function () {
         // title: package_info.config.appname
     })
     // and load the index.html of the app.
-    browserWindow.loadURL('file://' + __dirname + '/contents.html') // webviewデバッグ
-    // browserWindow.loadURL('file://' + __dirname + '/index.html')
+    // browserWindow.loadURL('file://' + __dirname + '/contents.html') // webviewデバッグ
+    browserWindow.loadURL('file://' + __dirname + '/index.html')
 
     let application_menu = [{
         label: "Edit",
