f5c3cbbdca56c96067b7bf44bcc71e50ecb6e2b3
m-shibata <misaki.pink@gmail.com>
2017-02-09 11:53:53 +0900

    Suggestの最大表示件数を10->20に変更

--- js/suggest-setting.js
+++ js/suggest-setting.js
@@ -3,12 +3,12 @@
 //   'MySQL', 'Oracle', 'PostgreSQL',"Hoge"];
 // var suggestList = ["Excel/Excelのセル内改行文字列の正規化/regexp.json", "SQL/INSERT文作成/regexp.json", "Text/空行削除/regexp.json", "Text/空行削除（スペースのみも対象）/regexp.json", "Text/空行削除（スペース・タブのみも対象）/regexp.json", "Text/空行削除（タブのみも対象）/regexp.json", "サンプル/サンプル/regexp.json", "Excel/HTMLテンプレート流し込み用関数作成/regexp.json"]
 function startSuggest() {
-  console.log("startSuggest");
+  // console.log("startSuggest");
   new Suggest.Local(
     "input-regexp-search",    // 入力のエレメントID
     "suggest", // 補完候補を表示するエリアのID
     suggestList,      // 補完候補の検索対象となる配列
-    { dispMax: 10, interval: 200 }); // オプション
+    { dispMax: 20, interval: 200 }); // オプション
 }
 
 // window.addEventListener ?
